832cf196b86a88be8c66039ad77fef4127f37732
mbarison <eltorpe@hotmail.com>
2017-12-20 15:20:27 -0500

    switch off vacuum for the time being

--- docs/topics/examples/simple-event/simple-event.sh
+++ docs/topics/examples/simple-event/simple-event.sh
@@ -8,7 +8,7 @@ event_provider="simple-event"
 event_type="my_event"
 
 # This is github.com/deis/empty-testbed
-project_id="brigade-830c16d4aaf6f5490937ad719afd8490a5bcbef064d397411043ac"
+project_id="brigade-adaf165d35013179fb5d1e1e50e0ed7fc99cf00cae67ce389374d5"
 commit="master"
 
 
@@ -33,26 +33,26 @@ while : ; do
   # This will just print the system time for the system running the script.
   payload=$(date)
 
-  cat <<EOF | kubectl --namespace ${namespace} create -f -
-  apiVersion: v1
-  kind: Secret
-  metadata:
-    name: ${name}
-    labels:
-      heritage: brigade
-      project: ${project_id}
-      build: ${uuid}
-      commit: ${commit}
-      component: build
-  type: Opaque
-  data:
-    commit: $(echo -n "${commit}" | base64)
-    event_provider: $(echo -n "${event_provider}" | base64)
-    event_type: $(echo -n "${event_type}" | base64)
-    project_id: $(echo -n "${project_id}" | base64)
-    build_id: $(echo -n "${uuid}" | base64)
-    payload: $(echo -n "${payload}" | base64)
-    script: $(echo -n "${script}" | base64)
+  cat <<EOF | tee test.yaml | kubectl --namespace ${namespace} create -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: ${name}
+  labels:
+    heritage: brigade
+    project: ${project_id}
+    build: ${uuid}
+    commit: ${commit}
+    component: build
+type: Opaque
+data:
+  commit: $(echo -n "${commit}" | base64 -w0)
+  event_provider: $(echo -n "${event_provider}" | base64 -w0)
+  event_type: $(echo -n "${event_type}" | base64 -w0)
+  project_id: $(echo -n "${project_id}" | base64 -w0)
+  build_id: $(echo -n "${uuid}" | base64 -w0)
+  payload: $(echo -n "${payload}" | base64 -w0)
+  script: $(echo -n "${script}" | base64 -w0)
 EOF
   sleep 60
 done
